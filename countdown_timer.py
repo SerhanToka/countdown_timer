e4ce851dc9b2dfff1c8f558287a46c65f8261ba5
SerhanToka <serhantokaa@hotmail.com>
2025-11-03 23:01:18 +0300

    End of the project. Hours added, typo corrected and countdown done message updated.

--- countdown_timer.py
+++ countdown_timer.py
@@ -1,11 +1,14 @@
 import time
 
-choosen_time = int(input("Enter time: "))
+chosen_time = int(input("Enter time in second/s: "))
 
-for x in range(chosen_time, 0, -1): # or we could use reversed(range(0, choosen_time)) for reverse timer
+for x in range(chosen_time, 0, -1): # or we could use reversed(range(0, chosen_time)) for reverse timer
     seconds = x % 60
     minutes = int(x / 60) % 60
-    print(f"00:{minutes:02}:{seconds:02}")
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
     time.sleep(1)
 
-print(f"Your {choosen_time} second/s countdown is done!")
+time = f"{hours:02}:{minutes:02}:{seconds:02}"
+
+print(f"Your {time} countdown is done!")
